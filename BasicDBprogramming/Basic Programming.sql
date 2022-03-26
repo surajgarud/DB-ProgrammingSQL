4ff7c1d297077a4df4e3c24587881ce922d93ed1
Suraj Garud <surajgarud95@gmail.com>
2022-03-26 18:46:09 +0530

    Implemented Sections on storedProcedure

--- BasicDBprogramming/Basic Programming.sql
+++ BasicDBprogramming/Basic Programming.sql
@@ -17,13 +17,13 @@ create table Employee
 	('Suresh','Rana','VitthalNager','Pune'),
 	('Suraj','Garud','Hadapsar','Pune')
 
-select * from EmpDetails
+select * from Employee
  
  --Update Table
 
- update Employee
- set Gender ='Male'
- where EmployeeID= 1
+ update EmpDetails
+ set salary ='26759'
+ where EmployeeID= 4
 
 
  ---Delete From Table
@@ -33,8 +33,8 @@ select * from EmpDetails
 
  --Alter Table
 
- alter table employee
- add MobileNo varchar(13)
+ alter table EmpDetails
+ add salary money
 
 alter table employee
  add Gender varchar(6)
@@ -46,13 +46,13 @@ alter table employee
 
 
  select * from Employee
- where not FirstName='ram'
+ where FirstName='ram' or LastName='Tiwari'
 
  select * from Employee
  order by City
 
-select * from Employee
-where MobileNo is not null
+select count (*)  from Employee
+where LastName = 'Tiwari'
 
 
 select top 1 * from employee
@@ -64,3 +64,130 @@ select top 1 * from employee
 
 
 sp_rename 'DB Table','Employee'
+
+
+
+
+
+---------------------------------Section-1 ----------------------------------------------------------
+select FirstName from employee;								  ----retrieve data from a table----
+
+select * from Employee											 ---Get All Data From Table------------
+
+select * from Employee where City= 'Pune'						 ----sort the result using where Claus-------
+
+select City, COUNT (*) as Number from Employee where Address ='RamNager' group by City;-----Using OrderBy Claus-----
+
+
+select * from Employee where city= 'Aurangabad' order by city    -----Get Result Using Order By Claus---------------
+
+
+--------------------------------Section 2 -------------------------------------------------------------
+
+select * from Employee order by city   -----Get Result Using Order By Claus ascending order------
+
+select * from Employee order by city Desc  ---------Get Result Using Order By Claus Desccending order------
+
+select * from Employee order by len(city)                -----------sorted by the length------------
+
+--------------------------------Section 3 -------------------------------------------------------------
+
+select * from Employee order by city offset 2 row   -------------skip the first 2 products-------------
+
+select * from Employee order by city offset 1 row FETCH NEXT 2 ROWS only  ------------using fetch Keyword---
+
+
+--------------------------------Section 4 -------------------------------------------------------------
+
+select DISTINCT Address from Employee     ----------------Ignore Duplicate Values-----------
+
+select * from Employee where City= 'Pune'	  ---------------- Finding rows----------
+
+select * from Employee where City= 'Pune' and Gender='Male'  ------ Finding rows using AND keyword------
+
+select * from Employee where City= 'Pune' or Gender='Male'    ------Finding rows that meet any of two conditions------
+
+select FirstName,LastName from Employee WHERE FirstName LIKE 'S%' ---Using Like Keyword to find a string--
+
+--------------------------------Section 5 -------------------------------------------------------------
+
+-------------INNER JOIN------
+CREATE SCHEMA SR;            ----created schema--
+GO
+
+
+CREATE TABLE SR.candidates(
+    id INT PRIMARY KEY IDENTITY,                     -------------Creating Table------------
+    FullName VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE SR.employees(
+    id INT PRIMARY KEY IDENTITY,                     -------------Creating Second------------
+    FullName VARCHAR(100) NOT NULL
+);
+
+
+INSERT INTO                                    ----------------Inserting value into candidates table---------
+    SR.candidates(FullName)
+VALUES
+    ('sam Karan'),            
+    ('Lily Bush'),
+    ('Peter Drucker'),
+    ('Jane Doe');
+	                                       select * from  SR.candidates
+
+INSERT INTO                               --------------Inserting value into candidates table------------
+    SR.employees(FullName)
+VALUES
+    ('sam Karan'),
+    ('Jane Doe'),
+    ('Michael Scott'),
+    ('Jack Sparrow');
+	                                           select * from  SR.employees
+
+	SELECT  
+    c.id candidate_id,
+    c.FullName candidate_name,
+    e.id employee_id,
+    e.FullName employee_name                   -----------Making InnerJoin------------
+FROM 
+    SR.candidates c
+    INNER JOIN SR.employees e 
+        ON e.FullName = c.FullName;
+
+
+------------------Left Join----------------
+
+	SELECT  
+    c.id candidate_id,
+    c.FullName candidate_name,
+    e.id employee_id,
+    e.FullName employee_name                   -----------Making LeftJoin------------
+FROM 
+    SR.candidates c
+    Left JOIN SR.employees e 
+        ON e.FullName = c.FullName;
+
+------------------Right Join----------------
+	SELECT  
+    c.id candidate_id,
+    c.FullName candidate_name,
+    e.id employee_id,
+    e.FullName employee_name                   -----------Making RightJoin------------
+FROM 
+    SR.candidates c
+    Right JOIN SR.employees e 
+        ON e.FullName = c.FullName;
+
+
+
+------------------Full Join----------------
+	SELECT  
+    c.id candidate_id,
+    c.FullName candidate_name,
+    e.id employee_id,
+    e.FullName employee_name                   -----------Making FullJoin------------
+FROM 
+    SR.candidates c
+    Full JOIN SR.employees e 
+        ON e.FullName = c.FullName;
--- StoredProcedure/Stored pro CRUD Operation.sql
+++ StoredProcedure/Stored pro CRUD Operation.sql
@@ -1,50 +1,54 @@
 use DB_Programming
 
 CREATE PROCEDURE SelectAllCity
-AS
+AS                                                  -----Section 1------
 SELECT * FROM Employee
 GO;
-SelectAllCity
+SelectAllCity                   ---Any keyword Can use--
+exec SelectAllCity            ----
 ------------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------------
-create table EmpDetails
-(
+alter table EmpDetails
+(                                                 -----storedProcedure with parameters----
     EmployeeID int identity(1,1) primary key,
-    FirstName varchar(255),
+    FirstName  varchar(255),
     LastName varchar(255),
     Address varchar(255),
     City varchar(255),
-	MobileNo varchar(13),
-	Gender varchar(6)     
+    MobileNo varchar(13),
+	Gender varchar(6),
+	Salary money not null
+ 
 );
 --------------------------------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------------------------
 ------------------------------Create And Insert By StoresProcedure-------------------------------------
 
 
-create procedure SPEmployee   
+alter procedure SPEmployee   
 (   
     @FirstName varchar(255),
     @LastName varchar(255),
     @Address varchar(255),
     @City varchar(255),
 	@MobileNo varchar(13),
-	@Gender varchar(6)
+	@Gender varchar(6),
+	@Salary int
 )   
 as    
 Begin    
-    Insert into EmpDetails (FirstName,LastName,Address,City,MobileNo,Gender)    
+    Insert into EmpDetails (FirstName,LastName,Address,City,MobileNo,Gender,salary)    
     Values 
-			('sai','Kamble','Pune','pune','8567193155','FeMale')
+			(@FirstName,@LastName,@Address,@City,@MobileNo,@Gender,@Salary) 
 	 
 End
 SELECT * FROM EmpDetails
-EXEC SPEmployee @FirstName = '@FirstName',@LastName = '@LasteName',@Address='@Address',@City ='@City',@MobileNo = '@MobileNo',@Gender = '@Gender'
+EXEC SPEmployee @FirstName = '@FirstName',@LastName = '@LasteName',@Address='@Address',@City ='@City',@MobileNo = '@MobileNo',@Gender = '@Gender',@Salary='@Salary'
 
 -----------------------------------------------------------------------------------------------------------
 creste procedure GetAllEntries  
 as   
-Begin   
+Begin                       -----------------------it defines A Block where We execute SQL statement-----------
 select
 	
     FirstName,
@@ -55,7 +59,7 @@ select
 	Gender
 
  from  EmpDetails
-End
+End                  
 
 exec GetAllEntries;
 ---------------------------------------------------------------------------------------------------------------
@@ -64,25 +68,17 @@ exec GetAllEntries;
 
 alter procedure UpdateEmployee     
 (     
-     @FirstName varchar(255),
-    --@LastName varchar(255),
-    @Address varchar(255),
-    @City varchar(255),
-	--@MobileNo varchar(13),
-	@Gender varchar(6)   
+     @salary money   
 )     
 as     
 begin     
    Update EmpDetails      
-   set 
-	   FirstName=@FirstName,     
-	   Address=@Address,     
-	   City=@City,   
-	   Gender=@Gender     
-	   where EmployeeID= 4     
+   set                            -------------Assign Value we use SET------------
+	   Salary=@salary  
+	   where EmployeeID= 11
 End
 
-UpdateEmployee  @FirstName = 'Vikas',@Address='Borivali',@City ='Mumbai',@Gender = 'Male'
+UpdateEmployee  @salary='39145'
 
 SELECT * FROM EmpDetails
 
@@ -103,3 +99,126 @@ End
 DeleteEmployee @EmployeeID=10
 
 SELECT * FROM EmpDetails
+
+select City, COUNT (*) from EmpDetails WHERE Address='Hadapsar' GROUP BY Gender ORDER BY Gender;
+
+------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------
+Declare
+ @FirstName varchar(100)                          ---------------Declare valriables---------
+ 
+ set @FirstName = 'suraj'
+ select 
+  FirstName,
+  LastName,
+  City
+From EmpDetails where @FirstName= FirstName--,@LastName = LastName
+order by LastName
+
+--------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------
+-------------------------------------section 2(ControlFlowStatement)-------------------------------------------------------------------
+BEGIN
+    DECLARE @salary INT;
+
+    SELECT 
+       @salary='15000'
+    FROM                                      ------------------use of if else statement-----------
+      EmpDetails
+
+    SELECT  @salary
+
+    IF  @salary > 10000
+    BEGIN
+        PRINT 'Great! The Salary Is MoreThan 10,000';
+    END
+    ELSE
+    BEGIN
+        PRINT 'Salary is Less Than 10,000';
+    END
+END
+
+SELECT * FROM EmpDetails
+-----------------------------------------------------
+DECLARE @counter INT=0;
+                                   ------------------use Of While statement---------------------
+WHILE @counter <= 10
+BEGIN
+    PRINT @counter;
+    SET @counter = @counter+1;
+END
+
+------------------------------------------------------
+DECLARE @count INT=2;
+                                   ------------------use Of While statement with Break---------------------
+WHILE @count <= 10
+BEGIN
+    PRINT @count;
+    SET @count = @count+1;
+	IF @count >= 4
+        BREAK;
+END
+
+------------------------------------------------------
+DECLARE @coun INT=0;
+                                   ------------------use Of While statement with Continue---------------------
+WHILE @coun < 5	
+BEGIN
+    SET @coun = @coun+1;
+	IF @coun = 4
+        Continue;
+		PRINT @coun;
+END
+
+---------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------
+-------------------------------------------Section 3(Handling Exceptions)-------------------------------------
+
+create PROC divide
+(                                     --------------Creating Divide Procedure-----------------
+    @a int,
+    @b int,                            ------------------variables-----------------
+    @c int output
+) AS
+BEGIN
+    BEGIN TRY
+        SET @c = @a / @b;
+    END TRY
+    BEGIN CATCH
+        SELECT  
+            ERROR_NUMBER() AS ErrorNumber  
+            ,ERROR_SEVERITY() AS ErrorSeverity  
+            ,ERROR_STATE() AS ErrorState  
+            ,ERROR_PROCEDURE() AS ErrorProcedure  
+            ,ERROR_LINE() AS ErrorLine  
+            ,ERROR_MESSAGE() AS ErrorMessage;  
+    END CATCH
+END;
+GO
+
+DECLARE @s int;                      -----------------------calling Addition Procedure-------------------
+exec  divide 7,0,@s output
+print @s
+
+drop proc Multiplication
+
+--------------------------------------------------------
+
+DECLARE 
+    @ErrorMessage  NVARCHAR(4000), 
+    @ErrorSeverity INT, 
+    @ErrorState    INT;                   ---------------Raise Error With Try Catch Block--------------------
+
+BEGIN TRY
+    RAISERROR('Error occurred in the TRY block.', 17, 1);
+END TRY
+BEGIN CATCH
+    SELECT 
+        @ErrorMessage = ERROR_MESSAGE(), 
+        @ErrorSeverity = ERROR_SEVERITY(), 
+        @ErrorState = ERROR_STATE();
+
+    RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState);
+END CATCH;
+
+
